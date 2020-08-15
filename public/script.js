9b8adb86958575df015f232820f47d533f72e389
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-15 22:47:14 +0000

    mute and umute the audio

--- public/script.js
+++ public/script.js
@@ -78,3 +78,57 @@ const scrollToBottom = () => {
     var d = $('.main_chat_window')
     d.scrollTo(d.prop('scrollHeight'))
 }
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  }
+//   const playStop = () => {
+//     let enabled = myVideoStream.getVideoTracks()[0].enabled;
+//     if (enabled) {
+//       myVideoStream.getVideoTracks()[0].enabled = false;
+//       setPlayVideo()
+//     } else {
+//       setStopVideo()
+//       myVideoStream.getVideoTracks()[0].enabled = true;
+//     }
+//   }
+
+
+  const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+
+//   const setStopVideo = () => {
+//     const html = `
+//       <i class="fas fa-video"></i>
+//       <span>Stop Video</span>
+//     `
+//     document.querySelector('.main__video_button').innerHTML = html;
+//   }
+  
+//   const setPlayVideo = () => {
+//     const html = `
+//     <i class="stop fas fa-video-slash"></i>
+//       <span>Play Video</span>
+//     `
+//     document.querySelector('.main__video_button').innerHTML = html;
+//   }
